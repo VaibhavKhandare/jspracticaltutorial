3bc22506a446cfeca6b2187c2489d8cc33c54355
khandare.v <khandare.v@media.net>
2025-11-30 19:18:10 +0530

    update

--- ecommerce-backend/src/index.js
+++ ecommerce-backend/src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const productsRouter = require('./routes/products');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
